3de9eb653e424c46906d944a2a83657d2567bd8f
Y.Harada <legend20sun@gmail.com>
2023-11-18 18:05:53 +0900

    create app: emfood

---
+++ tginet_C18/emfood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmfoodConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'emfood'
--- tginet_C18/tginet_C18/settings.py
+++ tginet_C18/tginet_C18/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emfood.apps.EmfoodConfig',
 ]
 
 MIDDLEWARE = [
