9c1ce8413952f436f257d218dae1594e84ede945
MoriyaKousei <moriya.kosei.up@tut.jp>
2023-11-18 21:11:16 +0900

    superuser

--- tginet_C18/emfood/models.py
+++ tginet_C18/emfood/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Emfood(models.Model):
+    name = models.CharField(max_length=200)
+    num = models.IntegerField()
+    expire_date = models.DateTimeField(blank=True, null=True)
