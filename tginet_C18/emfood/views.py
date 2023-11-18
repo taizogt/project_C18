b96230437b7ac41b09d064a991cd15ffbecb05f7
Y.Harada <legend20sun@gmail.com>
2023-11-18 22:05:13 +0900

    非常食リストのview作成

--- tginet_C18/emfood/views.py
+++ tginet_C18/emfood/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
-
+from .models import Emfood
 
 def index(request):
-    return render(request, "emfood/index.html")
+    foodData = Emfood.objects.all()
+    header = ['ID', '非常食名', '保有数', '消費期限']
+    my_dict = {
+        'title': '保有非常食一覧',
+        'header': header,
+        'foodData': foodData,
+    }
+    return render(request, "emfood/index.html", my_dict)
