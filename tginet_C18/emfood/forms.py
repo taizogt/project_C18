cd0c1b5f51678ac8898ee489814ee95d3e96560a
Y.Harada <legend20sun@gmail.com>
2023-11-19 11:38:02 +0900

    日付選択方法の変更

--- tginet_C18/emfood/forms.py
+++ tginet_C18/emfood/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Emfood  # importに追加
+from django.forms import widgets, NumberInput  # importに追加
 
 class Emfoodadd(forms.ModelForm):
     class Meta:
         model = Emfood
         fields = ['name','num','expire_date']
+        # expire_dateをプルダウン式に変更
+        widgets = {
+            'expire_date': NumberInput(attrs={'type': 'date'})
+        }
