f94709541eb96aa098951289a431589563fe2520
yuzuumez <tadaimao78@gmail.com>
2023-11-19 10:56:00 +0900

    DB_user_limit_date

--- tginet_C18/emfood/models.py
+++ tginet_C18/emfood/models.py
@@ -5,4 +5,8 @@ from django.conf import settings
 class Emfood(models.Model):
     name = models.CharField(max_length=200)
     num = models.IntegerField()
+    # 時刻あり期限
     expire_date = models.DateTimeField(blank=True, null=True)
+    user=models.CharField(max_length=10, default="unknown")
+    # 時刻なし消費期限
+    limit_date=models.DateField(blank=True, null=True )
